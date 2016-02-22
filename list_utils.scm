4e3cd4e5187dbee366e631bbc6fbfc46bcea23be
hosstay <hosstay@gmail.com>
2016-02-22 14:24:22 -0600

    initial commit

---
+++ list_utils.scm
@@ -0,0 +1,49 @@
+;sample list for manipulation
+
+(define b '(3 (23 8 (44 5 3) 4 5) 32 3 (2 3)))
+
+
+;rev-list
+
+(define (rev-list lst)
+   (if (null? lst)
+      '()
+      (append (rev-list (cdr lst)) (list (car lst)))
+   )
+)
+
+
+;rev-all-list
+
+(define (rev-all-lst lst)
+   (if (null? lst)
+      '()
+      (begin
+         (if atom? (car lst))
+            (append (rev-all-list (cdr lst)) (list (car lst)))
+            (append (rev-all-list (cdr lst)) (list (rev-all-list (car lst))))
+         )
+      )
+   )
+)
+
+
+;del-from-list
+
+(define (del-from-list itm lst)
+   (if (null? lst)
+      '()
+      (begin
+         (if (atom? (car lst))
+            (begin
+               (if (equal? (car lst) itm)
+                  (del-from-list itm (cdr lst))
+                  (append (list (car lst)) (del-from-list itm (cdr lst)))
+               )
+            )
+            (append (list (del-from-list itm (car lst)))
+                    (del-from-list itm (cdr lst)))
+         )
+      )
+   )
+)
